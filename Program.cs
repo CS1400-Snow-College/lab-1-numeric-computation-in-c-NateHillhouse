e01bbbe638a3c400e3f771d12dd82ef124bd3934
mizterwaffles <nhillhouse04@gmail.com>
2025-08-31 17:07:47 -0600

    Second step

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿int a = 5;
 int b = 6;
 int c = 7;
-Console.WriteLine($"{a}+{b}={a + b}");
+Console.WriteLine($"{a} + {b} = {a + b}");
+
+int d = a * b-c ;
+Console.WriteLine($"{a} * {b} - {c} = {d}");
